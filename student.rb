0d81dbff68cecafd7cb21d65f0806a566e9aa742
emmanuelsteven <write2stevemmy@gmail.com>
2023-07-12 13:02:21 +0100

    Update classes

---
+++ student.rb
@@ -0,0 +1,12 @@
+require './person'
+
+class Student < Person
+  def initialize(age, classrooms, name: 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @classrooms = classrooms
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
